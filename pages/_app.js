a032a3018c021e8b5317e7f23bf1e2d0cc006d33
Uri Shaked <uri@urishaked.com>
2022-01-05 19:47:43 +0200

    Add google login through firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { FirebaseAppProvider } from 'reactfire';
+import '../styles/globals.css';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCKBgG-vsqqTkhiaeXA9XRoyTVN8DxfAMY',
+  authDomain: 'practical-votes.firebaseapp.com',
+  projectId: 'practical-votes',
+  storageBucket: 'practical-votes.appspot.com',
+  messagingSenderId: '915771046953',
+  appId: '1:915771046953:web:a22a3843faa742773b7038',
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Component {...pageProps} />
+    </FirebaseAppProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
